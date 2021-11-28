1a27b37fa104e57c7f8a418686e74fd307dd9c63
Jonas <17405009+joblo2213@users.noreply.github.com>
2021-11-28 19:12:38 +0100

    Add TODO and update task template

--- templates/Task.java
+++ templates/Task.java
@@ -3,10 +3,12 @@ package io.github.joblo2213.adventofcode.y2021.day${day2D};
 import io.github.joblo2213.adventofcode.y2021.helpers.PuzzleInput;
 import io.github.joblo2213.adventofcode.y2021.helpers.Task;
 
+import static io.github.joblo2213.adventofcode.y2021.helpers.TODO.*;
+
 public class Day${day2D}${ch} extends Task{
     @Override
     public long run(PuzzleInput input) throws Exception{
         //TODO Implement
-        return -1;
+        return TODO();
     }
 }
