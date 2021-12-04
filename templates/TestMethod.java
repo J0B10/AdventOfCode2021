1fcea5cbb433fbb2abd45401d1d56c08bbeaf5a8
Jonas <17405009+J0B10@users.noreply.github.com>
2021-12-04 11:01:03 +0100

    Create unit test template

---
+++ templates/TestMethod.java
@@ -0,0 +1,6 @@
+@Test
+public void $NAME$() throws Exception{
+        org.junit.jupiter.api.Assertions.assertEquals($EXPECTED$, new $CLASS$.run(PuzzleInput.ofMulti("""
+            $END$
+            """)));
+        }
