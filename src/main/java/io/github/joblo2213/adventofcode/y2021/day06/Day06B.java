fab82c049a2a8f2bd786701ec76c873b2e333025
Jonas <17405009+J0B10@users.noreply.github.com>
2021-12-06 15:40:59 +0100

    🧠 Solve part 2 with elegance while brute force is slowly running out of RAM 🗿

--- src/main/java/io/github/joblo2213/adventofcode/y2021/day06/Day06B.java
+++ src/main/java/io/github/joblo2213/adventofcode/y2021/day06/Day06B.java
@@ -3,18 +3,33 @@ package io.github.joblo2213.adventofcode.y2021.day06;
 import io.github.joblo2213.adventofcode.y2021.helpers.PuzzleInput;
 import io.github.joblo2213.adventofcode.y2021.helpers.Task;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class Day06B extends Task {
+
+    private final int cycles;
+
+    public Day06B(int cycles) {
+        this.cycles = cycles;
+    }
+
+    public Day06B() {
+        this(256);
+    }
+
     @Override
     public long run(PuzzleInput input) throws Exception {
-        return input.withDelimiter(",").parallel().ints().mapToObj(IntStream::of).flatMapToInt(population -> {
-            for (int i = 0; i < 256; i++) {
-                population = population
-                        .map(fish -> fish - 1)
-                        .flatMap(fish -> fish == -1 ? IntStream.of(6, 8) : IntStream.of(fish));
-            }
-            return population;
-        }).count();
+        final long[] population = new long[9];
+        for (int i : input.withDelimiter(",").getInts()) population[i]++;
+        for (int i = 0; i < cycles; i++) updatePopulation(population);
+        return Arrays.stream(population).sum();
+    }
+
+    @SuppressWarnings("SuspiciousSystemArraycopy")
+    static void updatePopulation(long[] population) {
+        long reproducing = population[0];
+        System.arraycopy(population, 1, population, 0, 8);
+        population[6] += reproducing;
+        population[8] = reproducing;
     }
 }
--- src/test/java/io/github/joblo2213/adventofcode/y2021/day06/Day06B_Test.java
+++ src/test/java/io/github/joblo2213/adventofcode/y2021/day06/Day06B_Test.java
@@ -1,16 +1,36 @@
 package io.github.joblo2213.adventofcode.y2021.day06;
 
-import org.junit.jupiter.api.Disabled;
+import io.github.joblo2213.adventofcode.y2021.helpers.PuzzleInput;
 import org.junit.jupiter.api.Test;
 
 import static io.github.joblo2213.adventofcode.y2021.helpers.PuzzleInput.ofMulti;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled("Brute force")
 public class Day06B_Test {
 
     @Test
     public void test1() throws Exception {
+        //Test with example for part 1
+        PuzzleInput input = ofMulti("""
+                3,4,3,1,2
+                """);
+        assertEquals(5, new Day06B(1).run(input));
+        assertEquals(6, new Day06B(2).run(input));
+        assertEquals(7, new Day06B(3).run(input));
+        assertEquals(9, new Day06B(4).run(input));
+        assertEquals(10, new Day06B(5).run(input));
+        //....
+        assertEquals(10, new Day06B(8).run(input));
+        assertEquals(11, new Day06B(9).run(input));
+        //...
+        assertEquals(26, new Day06B(18).run(input));
+        //...
+        assertEquals(5934, new Day06B(80).run(input));
+    }
+
+    @Test
+    public void test2() throws Exception {
+        //Test with example for part 2
         assertEquals(26984457539L, new Day06B().run(ofMulti("""
                 3,4,3,1,2
                 """)));
