b01cc2b657671c13db049ad7f1914fc9df762963
Jonas <17405009+J0B10@users.noreply.github.com>
2021-12-14 11:55:00 +0100

    Enable all unit test

--- src/test/java/io/github/joblo2213/adventofcode/y2021/day14/Day14A_Test.java
+++ src/test/java/io/github/joblo2213/adventofcode/y2021/day14/Day14A_Test.java
@@ -1,7 +1,6 @@
 package io.github.joblo2213.adventofcode.y2021.day14;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import static io.github.joblo2213.adventofcode.y2021.helpers.PuzzleInput.ofMulti;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,7 +32,6 @@ public class Day14A_Test {
     }
 
     @Test
-    @DisabledIfEnvironmentVariable(named = "CI", matches = "true", disabledReason = "Disabled on GH Actions")
     public void task() throws Exception {
         new Day14A().run();
     }
